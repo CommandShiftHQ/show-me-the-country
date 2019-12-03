a9771333c0af399b202abe7953f7b76d825afb3d
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-12-03 16:00:53 +0000

    Complete app

--- src/components/country-card.jsx
+++ src/components/country-card.jsx
@@ -1,22 +1,49 @@
 import React from "react";
 import "../styles/CountryCard.css";
 
-const CountryCard = props => (
-  <div className="card-list">
-    <div className="card">
-      <img
-        src="https://i.redd.it/3y0ffehlprc11.png"
-        alt="flag"
-        style={{ width: "100%" }}
-      />
-      <div className="card-details">
-        <h4>
-          <b>I am a country</b>
-        </h4>
-        <p>These are some details about me</p>
+const CountryCard = props => {
+  const {
+    country: { name, region, capital, population, altSpellings, alpha2Code }
+  } = props;
+
+  return (
+    <div className="card-list">
+      <div className="card">
+        <img
+          src={`https://www.countryflags.io/${alpha2Code}/shiny/64.png`}
+          alt="flag"
+          style={{ width: "100%" }}
+        />
+        <div className="card-details">
+          <h2>
+            <b>{name}</b>
+          </h2>
+          <ul className="country-details">
+            <li>
+              <b>Region: </b>
+              {region}
+            </li>
+            <li>
+              <b>Capital: </b>
+              {capital}
+            </li>
+            <li>
+              <b>Population: </b>
+              {population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </li>
+            <li>
+              <b>AKA: </b>
+              <ol>
+                {altSpellings.map((spelling, i) => (
+                  <li key={i}>{spelling}</li>
+                ))}
+              </ol>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CountryCard;
